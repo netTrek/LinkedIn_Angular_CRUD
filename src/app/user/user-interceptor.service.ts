2bbfd64f5266c181b5bf1e03bd4be34635b8ff1c
producer <producer@highsierra1.content.linkedin.biz>
2019-04-12 11:26:40 +0200

    05_03_intercept_response

--- src/app/user/user-interceptor.service.ts
+++ src/app/user/user-interceptor.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+let reqCounter = 0;
 
 @Injectable ( {
   providedIn: 'root'
@@ -13,6 +16,22 @@ export class UserInterceptorService implements HttpInterceptor {
   intercept( req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
     return next.handle ( req.clone ( {
       setParams: { Test: 'netTrek' }
-    } ) );
+    } ) )
+               .pipe (
+                 tap ( event => {
+                     if ( event.type === HttpEventType.Sent ) {
+                       reqCounter ++;
+                       console.log ( 'SENT::: current num of requests ' + reqCounter );
+                     } else if ( event.type === HttpEventType.Response ) {
+                       reqCounter --;
+                       console.log ( 'RESPONSE::: current num of requests ' + reqCounter );
+                     }
+                   },
+                   err => {
+                     reqCounter --;
+                     console.log ( 'ERROR::: current num of requests ' + reqCounter );
+                   } )
+               )
+      ;
   }
 }
